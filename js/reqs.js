bc5ce3ac75640423db488a2ddcea1e6438887a97
Gavin Lloyd <gavinhungry@gmail.com>
2013-02-03 22:52:10 -0800

    first unit tests

--- js/reqs.js
+++ js/reqs.js
@@ -4,7 +4,7 @@ yepnope([
     nope: '//cdnjs.cloudflare.com/ajax/libs/json3/3.2.4/json3.min.js'
   }, {
     test: window.atob && window.btoa,
-    nope: '/js/base64.min.js', // github.com/davidchambers/Base64.js
+    nope: '/js/base64.min.js' // github.com/davidchambers/Base64.js
   },
   '//cdnjs.cloudflare.com/ajax/libs/jquery/1.9.0/jquery.min.js',
   '//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.4.4/underscore-min.js',
