a62251be92008ccff503424f8d562d4f6e4074db
Gavin Lloyd <gavinhungry@gmail.com>
2013-07-06 19:06:02 -0700

    update jquery to 1.10.2, underscore to 1.5.0

--- js/reqs.js
+++ js/reqs.js
@@ -11,8 +11,8 @@ yepnope([
     test: window.atob && window.btoa,
     nope: '/js/base64.min.js' // github.com/davidchambers/Base64.js
   },
-  '//cdnjs.cloudflare.com/ajax/libs/jquery/1.9.1/jquery.min.js',
-  '//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.4.4/underscore-min.js',
+  '//cdnjs.cloudflare.com/ajax/libs/jquery/1.10.2/jquery.js',
+  '//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.5.0/underscore-min.js',
   '//cdnjs.cloudflare.com/ajax/libs/backbone.js/1.0.0/backbone-min.js',
   '/js/slade.min.js'
 ]);
