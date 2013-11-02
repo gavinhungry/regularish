30cb2cf1a5b51b3ee898d3178be0ac345ee4915f
Gavin Lloyd <gavinhungry@gmail.com>
2013-11-02 15:04:57 -0700

    use minified jQuery

--- js/reqs.js
+++ js/reqs.js
@@ -11,7 +11,7 @@ yepnope([
     test: window.atob && window.btoa,
     nope: '/js/base64.min.js' // github.com/davidchambers/Base64.js
   },
-  '//cdnjs.cloudflare.com/ajax/libs/jquery/1.10.2/jquery.js',
+  '//cdnjs.cloudflare.com/ajax/libs/jquery/1.10.2/jquery.min.js',
   '//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.5.2/underscore-min.js',
   '//cdnjs.cloudflare.com/ajax/libs/backbone.js/1.1.0/backbone-min.js',
   '/js/slade.min.js'
